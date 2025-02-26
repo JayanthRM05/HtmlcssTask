e5a940a1f1f2344b49edf7efd0e97d485e9bbd34
“JayanthRM05” <jayanth.rapeti@hiringhood.com>
2025-02-27 02:07:41 +0530

    Responsive demo using react and js

---
+++ W3D2_25_Feb/responsive/ResponsiveWebsite/src/App.jsx
@@ -0,0 +1,26 @@
+
+import "./App.css";
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="Main">
+      <nav>
+        <h1>Header</h1>
+      </nav>
+     <div className="container">
+      <div className="one"><h1>left</h1></div> 
+      <div className="two"><h1>Main</h1></div>
+      <div className="three"><h1>Right</h1></div>
+     </div>
+     <div className="footer">
+      <h1>Footer</h1>
+      </div>
+     </div>
+    </>
+  );
+}
+
+export default App;
